9f8704680e9d8a7047e8d10421252d2d5aa08168
Kait860 <schneiderkaitlyn04@gmail.com>
2025-01-21 00:44:21 -0600

    Delete ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,9 +1,1 @@
-public class ArrayList {
-    public static void main(String[] args) throws Exception {
-        //creates an arrayList calles students
-        ArrayList<Student> students = new ArrayList<Student>();
 
-        //adds students to list
-        students.add(001, "Xaden", "123 Alphabet St." );
-    }
-}
